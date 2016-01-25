d94a2d298e8bb196cad9c4b69a745e1b58b97819
Kyle Bartz <kyle@unleashurgeek.com>
2016-01-25 10:33:28 -0800

    Initial Commit

---
+++ desktop/src/edu/uci/vgdc/proceduralgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.uci.vgdc.proceduralgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.uci.vgdc.proceduralgame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
