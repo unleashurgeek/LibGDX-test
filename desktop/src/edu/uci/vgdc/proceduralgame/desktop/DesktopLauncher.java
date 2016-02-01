effd8e38b207660e9ddd3167d0154604012dbfbb
Kyle Bartz <kyle@unleashurgeek.com>
2016-01-31 17:37:56 -0800

    Enabled vsync and set base window startup size.

--- desktop/src/edu/uci/vgdc/proceduralgame/desktop/DesktopLauncher.java
+++ desktop/src/edu/uci/vgdc/proceduralgame/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import edu.uci.vgdc.proceduralgame.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Procedural Game";
+		config.width = 1280;
+		config.height = 720;
+		config.vSyncEnabled = true; 
 		new LwjglApplication(new MainGame(), config);
 	}
 }
